842f721e55dea054083b20b71f65ddf645cf24ff
smvolokh <sema.volokh@yandex.ru>
2024-10-31 14:53:23 +0300

    Разбор первой программы по взрослому [#505151]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(String.format("1 * %d = %d", i, i));
+        }
+    }
+}
